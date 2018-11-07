fda69089609ba3f12d6a56c6cf43e57e9995977d
Saarim Rahman <saarimrahman@berkeley.edu>
2018-11-06 16:12:17 -0800

    updated basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button';
+import Info from './components/info';
+import Form from './components/form';
+import MainComponent from './components/styles';
+import Input from './components/input';
+import Header from './components/header';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style = {MainComponent}>
+        <main>
+          <Header />
+          <Form />
+          <p> Please send your {Info.coin} to {Info.ourPublicAddress}. </p>
+          <p> Your {Info.ogCoinValue} {Info.coin} will be transformed to approximately {Info.otherCoinValue} {Info.otherCoin} </p>
+
+          <p><strong> Note that in this preliminary outline of the website,
+          <li>Initial coin represents the coin that the user wants to send</li>
+          <li>Final coin represents the coin that the receiver wants </li>
+
+          </strong></p>
+        </main>
       </div>
     );
   }
 }
+
+
 
 export default App;
