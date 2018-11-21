39e5f28f8a98ad18ebcf68800eb2e1a4dc2282e4
Sushant Thyagaraj <sushantthyagaraj@sushant5inchmbp.attlocal.net>
2018-11-21 00:45:23 -0800

    Implementation of ShapeShift API in Python.

---
+++ backend/src/binance.js
@@ -0,0 +1,27 @@
+// Binance as an exchange, does not look viable as of right now so IGNORE
+
+const binance = require('node-binance-api')().options({
+APIKEY: 'Lhmyb5sLBqjJrpjRWSc1H2hQQ5vSuwINRGiOiadpMcU7858nnLKo9S0i9XboVHxp',
+APISECRET: 'Acw4KZVfu4uZLuo3jgeobJfceFIs5zWMPWOcGAsExUfEWYMMop07ASKHZET8hQ5m',
+useServerTime: true,
+test: true
+});
+
+//const instance1 = new binance().options({
+
+//});
+
+//const instance2 = new binance().options({
+//});
+
+binance.prices((error, ticker) => {
+  console.log("prices(): ", ticker);
+  console.log('Price of BTC:', ticker.BTCUSDT);
+});
+
+binance.useServerTime(() =>
+binance.balance((error, balances) => {
+  if ( error ) return console.error(error.body);
+  console.log("balances()", balances);
+  if ( typeof balances.ETH !== "undefined" ) console.log("ETH balance: ", balances.ETH.available);
+}));
---
+++ backend/src/coinbase.js
@@ -0,0 +1,9 @@
+// Still has to be finished, if we are going to continue to use coinbase
+
+var Client = require('coinbase').Client;
+
+var client = new Client({
+  'apiKey':
+  'apiSecret':
+  'version':
+})
